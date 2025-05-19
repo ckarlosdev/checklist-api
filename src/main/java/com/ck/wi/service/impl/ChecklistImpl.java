d8572a704a21ec3a7e62765ab18398ad5720af5a
Carlos Perez <per.ckarlos@gmail.com>
2025-05-19 14:55:14 -0500

    status forgoten (fixed)

--- src/main/java/com/ck/wi/service/impl/ChecklistImpl.java
+++ src/main/java/com/ck/wi/service/impl/ChecklistImpl.java
@@ -66,7 +66,7 @@ public class ChecklistImpl implements IChecklist {
                     .createdBy(employee.get().getFirstName() +" "+employee.get().getLastName())
                     .updatedBy("")
                     .updatedDate(today)
-                    .status(checklistDto.getStatus())
+                    .status("1")
                     .build();
 
             return checklistDao.save(checklist);
