92684c4b9cb19d6dd52bff42639a15d29b8580d2
ckarlosdev <ckarlos_perez@hotmail.com>
2026-01-15 14:41:35 -0600

    Disable form login and HTTP basic auth
    Disable form login and HTTP basic authentication in SecurityConfig.

--- src/main/java/com/ck/wi/SecurityConfig.java
+++ src/main/java/com/ck/wi/SecurityConfig.java
@@ -27,7 +27,9 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
-                );
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable());;
         return http.build();
     }
 
