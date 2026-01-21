bb17cfdbec8462943b8e4344bc39ce36aa894ab4
ckarlosdev <ckarlos_perez@hotmail.com>
2026-01-21 09:28:06 -0600

    Add new allowed origin for CORS configuration
    addin , "https://oleo-soft.com" permissions

--- src/main/java/com/ck/wi/SecurityConfig.java
+++ src/main/java/com/ck/wi/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("https://ckarlosdev.github.io"));
+        config.setAllowedOrigins(Arrays.asList("https://ckarlosdev.github.io", "https://oleo-soft.com"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
