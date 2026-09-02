f43c15f7077f739a73dfb9d361651f8934de2f14
Carlos Perez <per.ckarlos@gmail.com>
2026-09-02 09:38:53 -0500

    fix saving creating data

--- src/main/java/com/ck/wi/HmbrandtApiRestApplication.java
+++ src/main/java/com/ck/wi/HmbrandtApiRestApplication.java
@@ -2,8 +2,10 @@ package com.ck.wi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HmbrandtApiRestApplication {
 
 	public static void main(String[] args) {
